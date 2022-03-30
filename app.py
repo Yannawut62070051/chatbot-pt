986ff83214784d46cf370416d18b12d50832e014
Yannawut62070051 <56510625+Yannawut62070051@users.noreply.github.com>
2022-03-30 18:03:14 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,64 +1,79 @@
+from flask import make_response
+from flask import request
+from flask import Flask
+import os
+import json
+from asyncio import constants
 from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 
-import json
-import os
-from flask import Flask
-from flask import request
-from flask import make_response
-
 # Flask
 app = Flask(__name__)
-@app.route('/', methods=['POST'])
 
+
+@app.route('/', methods=['POST'])
 def MainFunction():
 
-    #รับ intent จาก Dailogflow
-    question_from_dailogflow_raw = request.get_json(silent=True, force=True)
+    # รับ intent จาก Dailogflow
+    rawMessage = request.get_json(silent=True, force=True)
 
-    #เรียกใช้ฟังก์ชัน generate_answer เพื่อแยกส่วนของคำถาม
-    answer_from_bot = generating_answer(question_from_dailogflow_raw)
-    
-    #ตอบกลับไปที่ Dailogflow
-    r = make_response(answer_from_bot)
-    r.headers['Content-Type'] = 'application/json' #การตั้งค่าประเภทของข้อมูลที่จะตอบกลับไป
+    # เรียกใช้ฟังก์ชัน generate_answer เพื่อแยกส่วนของคำถาม
+    bot_respone = generating_answer(rawMessage)
+
+    # ตอบกลับไปที่ Dailogflow
+    print(bot_respone)
+    r = make_response(bot_respone)
+    # การตั้งค่าประเภทของข้อมูลที่จะตอบกลับไป
+    r.headers['Content-Type'] = 'application/json'
 
     return r
 
-def generating_answer(question_from_dailogflow_dict):
 
-    #Print intent ที่รับมาจาก Dailogflow
-    print(json.dumps(question_from_dailogflow_dict, indent=4 ,ensure_ascii=False))
+def generating_answer(message):
 
-    #เก็บค่า ชื่อของ intent group ที่รับมาจาก Dailogflow
-    intent_group_question_str = question_from_dailogflow_dict["queryResult"]["intent"]["displayName"] 
-    print(intent_group_question_str)
+    # Print intent ที่รับมาจาก Dailogflow
+    # print(json.dumps(message, indent=4 ,ensure_ascii=False))
 
-    #ลูปตัวเลือกของฟังก์ชั่นสำหรับตอบคำถามกลับ
-    if intent_group_question_str == 'grade - custom':
-        answer_str = grade_calculator(question_from_dailogflow_dict)
-    elif intent_group_question_str == 'บวก':
-        answer_str = "พร้อมบวกแล้ว"
-    else: 
-        answer_str = "พูดไรนิ"
+    # เก็บค่า ชื่อของ intent group ที่รับมาจาก Dailogflow
+    intentGroup = message["queryResult"]["intent"]["displayName"]
+    print("intnent group: ", intentGroup)
 
-    #สร้างการแสดงของ dict 
-    answer_from_bot = {"fulfillmentText": answer_str}
-    
-    #แปลงจาก dict ให้เป็น JSON
-    answer_from_bot = json.dumps(answer_from_bot, indent=4) 
-    
-    return answer_from_bot
+    # ลูปตัวเลือกของฟังก์ชั่นสำหรับตอบคำถามกลับ
+    if intentGroup == 'grade - custom':
+        answer = grade_calculator(message)
+    elif intentGroup == 'บวก':
+        answer = "พร้อมบวกแล้วน้าาา"
+    elif intentGroup == 'gpsTest':
+        return json.dumps({
+            "address": "2194 ถนน เจริญกรุง แขวง วัดพระยาไกร เขตบางคอแหลม กรุงเทพมหานคร 101200",
+            "title": "เอเชียทีค เดอะ ริเวอร์ฟร้อนท์",
+            "latitude": 13.704435,
+            "type": "location",
+            "longitude": 100.503212
+        },ensure_ascii=False)
+    elif intentGroup == 'imgTest':
+        return json.dumps({"fulfillmentText": "ลองรูป"})
+    else:
+        answer = "พูดไรนิ"
+
+    # สร้างการแสดงของ dict
+    botResponse = {"fulfillmentText": answer}
+
+    # แปลงจาก dict ให้เป็น JSON
+    botResponse = json.dumps(botResponse)
+
+    return botResponse
+
 
 def grade_calculator(res):
-    score = float(res["queryResult"]["outputContexts"][1]["parameters"]["score.original"])
+    score = float(res["queryResult"]["outputContexts"]
+                  [1]["parameters"]["score.original"])
     if score == 99:
         return "หูยเก่งจุง"
-    return "คะแนนเท่านี้แน่นะ "+str(score)
+    return "อ่อนเกิ้น"
 
 
-#กำหนด port ให้ flask
+# กำหนด port ให้ flask
 if __name__ == '__main__':
-     app.run(threaded=True, port=5000)
-
+    app.run(threaded=True, port=5000)
